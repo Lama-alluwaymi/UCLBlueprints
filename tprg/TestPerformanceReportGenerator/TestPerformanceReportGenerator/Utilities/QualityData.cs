a13e3cf6f076336e7a0e9d5f67816840c4577c09
SimonL26 <806985104@qq.com>
2022-08-20 15:11:54 +0100

    Moved TestTemplate.html into utilities folder, added file for report generation integration

---
+++ tprg/TestPerformanceReportGenerator/TestPerformanceReportGenerator/Utilities/QualityData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPerformanceReportGenerator.Utilities
+{
+    public class QualityData
+    {
+        public string Date { get; set; }
+        public string Version { get; set; }
+        public string Hardware { get; set; }
+        public string Failed { get; set; }
+        public string Passed { get; set; }
+        public string TestCases { get; set; }
+        public string LineCoverage { get; set; }
+        public string Duration { get; set; }
+        public string Devices { get; set; }
+    }
+}
---
+++ tprg/TestPerformanceReportGenerator/TestPerformanceReportGenerator/Utilities/ReportGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPerformanceReportGenerator.Utilities
+{
+    public class ReportGenerator
+    {
+        public List<QualityData> dataList;
+
+        public void AddTestData(string date, string version, string hardware, string failedTests, 
+            string passedTests, string testCases, string lineCoverage, string testDuration, string device)
+        {
+            
+        }
+        public void GenerateReport(string projectName, List<QualityData> dataList, 
+            string maintainability, string cycloComplexity, string depthOfInheritnace, string classCoupling,
+            string losc, string loec)
+        {
+
+        }
+    }
+}
