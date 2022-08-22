6c7267e8feac05db0c2c054f06e8cf9cffda447c
SimonL26 <806985104@qq.com>
2022-08-22 03:50:11 +0100

    Added notifications and progress indications when test are automatically ran, when test run data is being added and when report is being generated

--- tprg/TestPerformanceReportGenerator/TestPerformanceReportGenerator/ToolWindows/MyToolWindowControl.xaml.cs
+++ tprg/TestPerformanceReportGenerator/TestPerformanceReportGenerator/ToolWindows/MyToolWindowControl.xaml.cs
@@ -2,6 +2,8 @@
 using System.Windows.Controls;
 using System.Reflection;
 using TestPerformanceReportGenerator.Utilities;
+using Microsoft.VisualStudio.Imaging;
+
 
 namespace TestPerformanceReportGenerator
 {
@@ -30,15 +32,19 @@ namespace TestPerformanceReportGenerator
         }
 
         /// <summary>
-        /// Function that is executed when run test button has been clicked.
+        /// Run test button click event handler.
         /// </summary>
         /// <param name="sender"></param>6
         /// <param name="e"></param>
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            // Auto run tests asynchronousely such that it won't block IDE's UI
             ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
             {
-                //TestAutoRunner autoRunner = new TestAutoRunner();
+                // Show progress in the IDE's status bar
+                VS.StatusBar.ShowMessageAsync("Running Test Cases").FireAndForget();
+                VS.StatusBar.StartAnimationAsync(StatusAnimation.General).FireAndForget();
+                // Run "autorun Test case" function and display in the tool window
                 await Task.Run(() => this.autoRunner.runTestCases());
                 total.Content = this.autoRunner.totalTests;
                 passed.Content = this.autoRunner.passedTest + "/" + this.autoRunner.totalTests;
@@ -50,19 +56,21 @@ namespace TestPerformanceReportGenerator
                 failed.Visibility = Visibility.Visible;
                 skipped.Visibility = Visibility.Visible;
                 totduration.Visibility = Visibility.Visible;
+                //Update status bar
+                VS.StatusBar.EndAnimationAsync(StatusAnimation.General).FireAndForget();
+                VS.StatusBar.ShowMessageAsync("Test Run Completed").FireAndForget();
             }).FireAndForget();
+
         }
 
         /// <summary>
-        /// Event handler for checkbox that will allow user to 
-        /// manually enter the relevant data. It disables the button that
-        /// automatically runs the test and will display all the input field.
+        /// Checkbox event handler for checkbox when checked
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void autoRunChecked(object sender, RoutedEventArgs e)
         {
-            //setting some attributes of the xaml objects.
+            //enable autorun button and hide inputs.
             autoTestBtn.IsEnabled = true;
             autoTestBtn.Visibility = Visibility.Visible;
             totalTestInput.Visibility = Visibility.Collapsed;
@@ -78,8 +86,14 @@ namespace TestPerformanceReportGenerator
 
         }
 
+        /// <summary>
+        /// Checkbox event handler for checkbox when unchecked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void autoRunUnchecked(object sender, RoutedEventArgs e)
         {
+            // disable autorun button and enable all inputs
             autoTestBtn.IsEnabled = false;
             autoTestBtn.Visibility = Visibility.Collapsed;
             totalTestInput.Visibility = Visibility.Visible;
@@ -94,83 +108,148 @@ namespace TestPerformanceReportGenerator
             totduration.Visibility = Visibility.Collapsed;
         }
 
+        /// <summary>
+        /// Generate report button click event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void submitData_Click(object sender, RoutedEventArgs e)
         {
-            this.maintainInputTxt = maintainabilityInput.Text;
-            this.cycloInputTxt = cyclomaticInput.Text;
-            this.depthOfInputTxt = depthInheritanceInput.Text;
-            this.classCInputTxt = classCouplingInput.Text;
-            this.loscInputTxt = loscInput.Text;
-            this.loecInputTxt = loecInput.Text;
-
-            //VS.MessageBox.Show("//", this.autoRunner.passedTest);
-            ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
-            {
-                await Task.Run(() =>
+            // set the info bar model to show up when report is generated
+            var model = new InfoBarModel(
+                new[]
                 {
-                    string projectName = Assembly.GetCallingAssembly().GetName().Name;
-                    ReportGenerator.GenerateReport(projectName, this.generator.dataList,
-                                                    this.maintainInputTxt, this.cycloInputTxt,
-                                                    this.depthOfInputTxt, this.classCInputTxt,
-                                                    this.loscInputTxt, this.loecInputTxt);
-                });
-                // Clearing all input after report generation
-                ClearQualityInputs();
-            }).FireAndForget();
+                    new InfoBarTextSpan("Report Successfully Generated")
+                },
+                KnownMonikers.CheckAdd,
+                true
+                );
+            // Check if all inputs are not empty
+            if (!maintainabilityInput.Text.Equals("") && !cyclomaticInput.Text.Equals("") &&
+                !depthInheritanceInput.Text.Equals("") && !classCouplingInput.Text.Equals("") &&
+                !loscInput.Text.Equals("") && !loecInput.Text.Equals(""))
+            {
+                this.maintainInputTxt = maintainabilityInput.Text;
+                this.cycloInputTxt = cyclomaticInput.Text;
+                this.depthOfInputTxt = depthInheritanceInput.Text;
+                this.classCInputTxt = classCouplingInput.Text;
+                this.loscInputTxt = loscInput.Text;
+                this.loecInputTxt = loecInput.Text;
+
+                // Run the generate report function asynchronously such that it won't block the UI
+                ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                {
+                    InfoBar infobar = await VS.InfoBar.CreateAsync("{ce2a0f92-d094-48c5-be02-b8624558136a}", model);
+                    await Task.Run(() =>
+                    {
+                        string projectName = Assembly.GetCallingAssembly().GetName().Name;
+                        ReportGenerator.GenerateReport(projectName, this.generator.dataList,
+                                                        this.maintainInputTxt, this.cycloInputTxt,
+                                                        this.depthOfInputTxt, this.classCInputTxt,
+                                                        this.loscInputTxt, this.loecInputTxt);
+                    });
+                    // Show infobar when report is generated
+                    await infobar.TryShowInfoBarUIAsync();
+                    // Clearing all input after report generation
+                    ClearQualityInputs();
+                    
+                }).FireAndForget();
+            }
         }
 
+        /// <summary>
+        /// Add test data button click event handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void addTestDataBtn_Click(object sender, RoutedEventArgs e)
         {
-            this.failedInputTxt = failedTestInput.Text;
-            this.passedInputTxt = passedTestInput.Text;
-            this.totaltestInputTxt = totalTestInput.Text;
-            this.skippedInputTxt = skippedTestInput.Text;
-            this.durationInputTxt = durationInput.Text;
-            this.coverageInputTxt = coverageInput.Text;
+            // set the infobar model to show up when the test run has been added
+            var model = new InfoBarModel(
+                    new[]{
+                        new InfoBarTextSpan("Test run successfully added to the report.")
+                    },
+                    KnownMonikers.AddAttachment,
+                    true);
 
+            //check if autorun test case is enabled
             if (chk_manual.IsChecked == true)
             {
+                // check if autorun test case has been performed
                 if (!total.Content.Equals("") && !passed.Content.Equals("") && !failed.Content.Equals("")
                     && !skipped.Content.Equals("") && !totduration.Content.Equals("") && !coverageInput.Text.Equals(""))
-                { 
+                {
+                    this.coverageInputTxt = coverageInput.Text;
+                    // add test run data asynchronously
                     ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
                     {
+                        InfoBar infobar = await VS.InfoBar.CreateAsync("{ce2a0f92-d094-48c5-be02-b8624558136a}", model);
                         await Task.Run(() =>
                         {
-                            // Get current Date
+                            // Get current date
                             string dateNow = SystemInfoRetriver.GetCurrentDate();
+                            // Get the project version
                             string version = SystemInfoRetriver.GetProductVersion();
+                            // Get hardware info of the user
                             string hardwareInfo = SystemInfoRetriver.GetHardwareInfo();
                             string dur = this.autoRunner.duration.Replace("ms", "");
                             this.generator.AddTestData(dateNow, version, hardwareInfo, this.autoRunner.failedTest, this.autoRunner.passedTest,
                                                     this.autoRunner.totalTests, this.coverageInputTxt, dur, this.autoRunner.skippedTest);
                         });
+                        // show info bar when data has been added
+                        await infobar.TryShowInfoBarUIAsync();
+                        ClearTestInputs();
+
+                    }).FireAndForget();
+                }
+                else
+                {
+                    VS.MessageBox.ShowError("Code Quality Report Generator", "Click 'Run Test!' button to run the test first.");
+                }
+            }
+            //If user manually enters data
+            else
+            {
+                // check all entry fields are not empty
+                if(!failedTestInput.Text.Equals("") && !passedTestInput.Text.Equals("") && !skippedTestInput.Text.Equals("")
+                    && !totalTestInput.Text.Equals("") && !durationInput.Text.Equals("") && !coverageInput.Text.Equals(""))
+                {
+                    this.failedInputTxt = failedTestInput.Text;
+                    this.passedInputTxt = passedTestInput.Text;
+                    this.totaltestInputTxt = totalTestInput.Text;
+                    this.skippedInputTxt = skippedTestInput.Text;
+                    this.durationInputTxt = durationInput.Text;
+                    this.coverageInputTxt = coverageInput.Text;
+
+                    ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
+                    {
+                        InfoBar infobar = await VS.InfoBar.CreateAsync("{ce2a0f92-d094-48c5-be02-b8624558136a}", model);
+                        await Task.Run(() =>
+                        {
+                            // Get date
+                            string dateNow = SystemInfoRetriver.GetCurrentDate();
+                            // Get project version
+                            string version = SystemInfoRetriver.GetProductVersion();
+                            // Get user hardware info
+                            string hardwareInfo = SystemInfoRetriver.GetHardwareInfo();
+                            this.generator.AddTestData(dateNow, version, hardwareInfo, this.failedInputTxt, this.passedInputTxt,
+                                this.totaltestInputTxt, this.coverageInputTxt, this.durationInputTxt, this.skippedInputTxt);
+
+                        });
+                        await infobar.TryShowInfoBarUIAsync();
                         ClearTestInputs();
                     }).FireAndForget();
                 }
                 else
                 {
-                    VS.MessageBox.Show("Code Quality Report Generator", "Click 'Run Test!' button to run the test first.");
+                    VS.MessageBox.ShowError("Code Quality Report Generator", "Please provide all the input in the form");
                 }
-            }
-            else
-            {
-                ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
-                {
-                    await Task.Run(() =>
-                    {
-                        string dateNow = SystemInfoRetriver.GetCurrentDate();
-                        string version = SystemInfoRetriver.GetProductVersion();
-                        string hardwareInfo = SystemInfoRetriver.GetHardwareInfo();
-                        this.generator.AddTestData(dateNow, version, hardwareInfo, this.failedInputTxt, this.passedInputTxt,
-                            this.totaltestInputTxt, this.coverageInputTxt, this.durationInputTxt, this.skippedInputTxt);
 
-                    });
-                    ClearTestInputs();
-                }).FireAndForget();
             }
         }
-
+        /// <summary>
+        /// Helper method to clear all input when test run data has been added to report
+        /// </summary>
         private void ClearTestInputs()
         {
             failedTestInput.Clear();
@@ -180,7 +259,9 @@ namespace TestPerformanceReportGenerator
             durationInput.Clear();
             skippedTestInput.Clear();
         }
-
+        /// <summary>
+        /// Helper method to clear all input when report has generated.
+        /// </summary>
         private void ClearQualityInputs()
         {
             maintainabilityInput.Clear();
