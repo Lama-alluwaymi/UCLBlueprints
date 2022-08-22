374d9aad86ad7e466ceb91abbbc321739f199d77
chris <chris.palmer10112@gmail.com>
2022-08-22 14:32:42 +0100

    Trello Report Generation

---
+++ Trello-Report-Generator/main.js
@@ -0,0 +1,34 @@
+function countCardsInList(listID, key, token) {
+    fetch("https://api.trello.com/1/lists/" + listID + "/cards?key=" + key + "&token=" + token + "&fields=name,shortUrl").then((data) =>{
+        return data.json();
+    }).then((objectData) => {
+        console.log(objectData[0].title);
+        let tableData="";
+        objectData.map((values) => {
+            tableData+=`<tr>
+            <td>${values.id}</td>lll
+            <td>${values.name}</td>
+            <td>${values.shortUrl}</td>
+            </tr>`;
+        });
+        document.getElementById("table_body").innerHTML=tableData;
+    }).catch((err) => {
+        console.log(err);
+    })
+
+}
+
+//var listID = document.getElementById("listID");
+//var token = document.getElementById("token");
+//var key = document.getElementById("key");
+
+//console.log(listID);
+//console.log(token);
+//console.log(key);
+
+//countCardsInList(listID, key, token);
+// listid= 62f67eb4babcd7227e1d7f3c
+ //key=c6740dc77e0524d9f6192299e0821c9c
+ //token=502942d216f43d75b1c9954158d49049c4cd74a20d1f0daade7c035489c3ba35
+ 
+
