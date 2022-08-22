75a7cf4ac227662eaec0597303b2561a011003c4
chris <chris.palmer10112@gmail.com>
2022-08-22 17:07:40 +0100

    added drop down box for list selection

--- Trello-Report-Generator/main.js
+++ Trello-Report-Generator/main.js
@@ -1,3 +1,25 @@
+//gets all the lists in a board
+function getListsForDropdown(boardID, key, token) {
+
+fetch("https://api.trello.com/1/boards/" + boardID + "/lists?key=" + key + "&token=" + token + "&fields=id,name").then((data) => {
+
+    return data.json();
+}).then((objectData) => {
+    console.log(objectData[0].title);
+    let dropDownData="";
+    objectData.map((values) => {
+        dropDownData+= `
+        <option value="${values.id}"> ${values.name}</option>
+        `;
+    });
+    document.getElementById("listDropDown").innerHTML=dropDownData;
+}).catch((err) => {
+    console.log(err);
+})
+
+}
+
+//gets all the cards in the list
 function countCardsInList(listID, key, token) {
     fetch("https://api.trello.com/1/lists/" + listID + "/cards?key=" + key + "&token=" + token + "&fields=name,shortUrl").then((data) =>{
         return data.json();
