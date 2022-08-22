9611c0a355caf00a0b5eea78a1f05a8bd56d113a
chris <chris.palmer10112@gmail.com>
2022-08-22 17:39:06 +0100

    urls for cards in report

--- Trello-Report-Generator/main.js
+++ Trello-Report-Generator/main.js
@@ -21,16 +21,19 @@ fetch("https://api.trello.com/1/boards/" + boardID + "/lists?key=" + key + "&tok
 
 //gets all the cards in the list
 function countCardsInList(listID, key, token) {
-    fetch("https://api.trello.com/1/lists/" + listID + "/cards?key=" + key + "&token=" + token + "&fields=name,shortUrl").then((data) =>{
+    fetch("https://api.trello.com/1/lists/" + listID + "/cards?key=" + key + "&token=" + token + "&fields=name,shortUrl,desc,dateLastActivity").then((data) =>{
         return data.json();
     }).then((objectData) => {
         console.log(objectData[0].title);
         let tableData="";
         objectData.map((values) => {
+            //values.id is current placeholder for who is assigned to the card, needs to be updated
             tableData+=`<tr>
-            <td>${values.id}</td>lll
             <td>${values.name}</td>
-            <td>${values.shortUrl}</td>
+            <td><a href="${values.shortUrl}">${values.shortUrl}</a></td>
+            <td>${values.desc}</td>
+            <td>${values.dateLastActivity}</td>
+            <td>${values.id}</td>
             </tr>`;
         });
         document.getElementById("table_body").innerHTML=tableData;
