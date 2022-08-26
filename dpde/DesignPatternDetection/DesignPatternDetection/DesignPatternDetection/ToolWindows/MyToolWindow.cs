549fe879ce8edb57fb92d5d417ec2d5df0693cf8
Lama-alluwaymi <63431277+Lama-alluwaymi@users.noreply.github.com>
2022-08-26 09:20:30 +0100

    initial commit - adding design pattern vs extension

---
+++ dpde/DesignPatternDetection/DesignPatternDetection/DesignPatternDetection/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace DesignPatternDetection
+{
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("e6c75ea4-09c5-4e32-bc0b-e221277993c1")]
+    public class MyToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyToolWindow"/> class.
+        /// </summary>
+        public MyToolWindow() : base(null)
+        {
+            this.Caption = "MyToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new MyToolWindowControl();
+        }
+    }
+}
---
+++ dpde/DesignPatternDetection/DesignPatternDetection/DesignPatternDetection/ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,71 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace DesignPatternDetection
+{
+    /// <summary>
+    /// Interaction logic for MyToolWindowControl.
+    /// </summary>
+    public partial class MyToolWindowControl : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MyToolWindowControl"/> class.
+        /// </summary>
+        public MyToolWindowControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private List<string> AvailablePatterns = new List<string>() { "SINGLETON","FACTORY","ABSTRACT FACTORY", "ABSTRACTFACTORY", "BUILDER", "PROTOTYPE"};
+
+        /// <summary>
+        /// Handles click on the button by displaying a message box.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        [SuppressMessage("Microsoft.Globalization", "CA1300:SpecifyMessageBoxOptions", Justification = "Sample code")]
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Default event handler naming pattern")]
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var dte = ServiceProvider.GlobalProvider.GetService(typeof(DTE)) as DTE2;
+            Assumes.Present(dte);
+            var proj = dte.Solution.Projects.OfType<EnvDTE.Project>().FirstOrDefault();
+            if (proj == null)
+            {
+                VS.MessageBox.Show("Please open any project");
+                return;
+            }
+            var filename = proj.FullName;
+            if (filename == null)
+                return;
+            string directory = Directory.GetParent(filename).FullName;
+            if (string.IsNullOrEmpty(pattern.Text))
+            {
+                VS.MessageBox.Show("Please enter the pattern to search");
+                return;
+            }
+            if (!AvailablePatterns.Contains(pattern.Text.Trim().ToUpper()))
+            {
+                VS.MessageBox.Show("Please enter a valid pattern to search");
+                return;
+            }
+            VS.MessageBox.Show($"Searching for pattern {pattern.Text} in {directory}");
+            // Pattern
+            // FileName
+            // List<Location>
+
+           
+            File.WriteAllText($"{directory}\\sample.json", "sample check");
+        }
+    }
+}
