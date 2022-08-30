d0849a38260387e5da51048ffe39bc4da8c59bef
SimonL26 <806985104@qq.com>
2022-08-30 15:29:23 +0100

    Changing display location of the extension

--- tprg/CodeQualityReportGen/CodeQualityReportGen/CodeQualityReportGenPackage.cs
+++ tprg/CodeQualityReportGen/CodeQualityReportGen/CodeQualityReportGenPackage.cs
@@ -12,7 +12,7 @@ namespace CodeQualityReportGen
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.CodeQualityReportGenString)]
     [ProvideToolWindow(typeof(CodeQualityToolWindow.Pane), Style = VsDockStyle.Tabbed, Window = WindowGuids.SolutionExplorer)]
-    [ProvideToolWindow(typeof(QualityOverviewToolWindow.Pane), Style = VsDockStyle.Float)]
+    [ProvideToolWindow(typeof(QualityOverviewToolWindow.Pane), Style = VsDockStyle.Linked, Window = PackageGuids.CodeQualityWindowString)]
     public sealed class CodeQualityReportGenPackage : ToolkitPackage
     {
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
