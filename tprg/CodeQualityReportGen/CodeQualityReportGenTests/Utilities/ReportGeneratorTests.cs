6183d0dd92ab2bebe1181c530543a3441f747cf6
SimonL26 <806985104@qq.com>
2022-08-30 01:05:54 +0100

    Added unit test

---
+++ tprg/CodeQualityReportGen/CodeQualityReportGenTests/Utilities/ReportGeneratorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeQualityReportGen.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeQualityReportGen.Utilities.Tests
+{
+    [TestClass()]
+    public class ReportGeneratorTests
+    {
+        [TestMethod()]
+        public void AddTestDataTest()
+        {
+            ReportGenerator generator = new ReportGenerator();
+            generator.AddTestData("00:00:00", "1.0", "hardware", "3", "3", "6", "100", "120", "0");
+            generator.AddTestData("00:00:00", "1.0", "hardware", "3", "3", "6", "100", "120", "0");
+            Assert.AreEqual(2, generator.dataList.Count);
+        }
+    }
+}
