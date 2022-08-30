265f4552beb4f726e32ea5bfeebab90615acda9e
Lama-alluwaymi <63431277+Lama-alluwaymi@users.noreply.github.com>
2022-08-30 08:49:55 +0100

    Added support for pdf generation

--- dpde/DesignPatternDetection/DesignPatternDetection/DesignPatternDetection/ToolWindows/MyToolWindowControl.xaml.cs
+++ dpde/DesignPatternDetection/DesignPatternDetection/DesignPatternDetection/ToolWindows/MyToolWindowControl.xaml.cs
@@ -9,6 +9,9 @@ using System.Windows.Controls;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
+using Aspose.Cells;
+using System.Data;
+using Style = Aspose.Cells.Style;
 
 namespace DesignPatternDetection
 {
@@ -110,6 +113,74 @@ namespace DesignPatternDetection
                 File.WriteAllText($"{directory}\\patterns.json", jsonData);
 
                 VS.MessageBox.Show($"Generated patterns.json in {directory}");
+
+                var result = VS.MessageBox.Show($"Do you want to generate patterns.pdf?");
+                if (result == Microsoft.VisualStudio.VSConstants.MessageBoxResult.IDOK)
+                {
+                    var workbook = new Workbook();
+                    var worksheet = workbook.Worksheets[0];
+
+                    var dt = new DataTable();
+
+                    dt.Columns.Add("Pattern", typeof(string));
+                    dt.Columns.Add("Total", typeof(string));
+                    dt.Columns.Add("File Name", typeof(string));
+                    dt.Columns.Add("Line", typeof(string));
+
+                    dt.Rows.Add(dt.NewRow());
+                    foreach (var pattern in patterns)
+                    {
+                        var dr = dt.NewRow();
+                        dr["Pattern"] = pattern.Pattern;
+                        dr["Total"] = pattern.totalCount.ToString();
+                        dr["File Name"] = string.Empty;
+                        dr["Line"] = string.Empty;
+                        dt.Rows.Add(dr);
+                        foreach (var entry in pattern.Entries)
+                        {
+                            dr = dt.NewRow();
+                            dr["Pattern"] = string.Empty;
+                            dr["Total"] = string.Empty;
+                            dr["File Name"] = entry.FileName;
+                            dr["Line"] = entry.Line;
+                            dt.Rows.Add(dr);
+                        }
+                        dr = dt.NewRow(); //empty row to differentiate
+                        dt.Rows.Add(dr);
+                    }
+
+                    Cells cells = worksheet.Cells;
+                    cells.UnMerge(0, 1, 2, 2);
+                    cells.Merge(0, 1, 2, 2);
+                    worksheet.Cells[0, 1].PutValue("DESIGN PATTERN REPORT");
+                    Style style = worksheet.Cells[0, 1].GetStyle();
+                    Aspose.Cells.Font font = style.Font;
+                    font.Size = 18;
+                    font.IsBold = true;
+                    style.Pattern = BackgroundType.Solid;
+                    style.HorizontalAlignment = TextAlignmentType.Center;
+                    cells[0, 1].SetStyle(style);
+
+                    worksheet.Cells.ImportDataTable(dt, true, "A8");
+                    worksheet.AutoFitColumn(0);
+                    worksheet.AutoFitColumn(1);
+                    worksheet.AutoFitColumn(2);
+                    worksheet.AutoFitColumn(3);
+                    worksheet.PageSetup.Orientation = PageOrientationType.Landscape;
+
+                    Style patternStyle = worksheet.Cells[7, 0].GetStyle();
+                    Aspose.Cells.Font patternFont = patternStyle.Font;
+                    patternFont.IsBold = true;
+                    patternStyle.Pattern = BackgroundType.Solid;
+                    patternStyle.HorizontalAlignment = TextAlignmentType.Center;
+                    cells[7, 0].SetStyle(patternStyle);
+                    cells[7, 1].SetStyle(patternStyle);
+                    cells[7, 2].SetStyle(patternStyle);
+                    cells[7, 3].SetStyle(patternStyle);
+
+                    workbook.Save($"{directory}\\patterns.pdf", Aspose.Cells.SaveFormat.Pdf);
+                    VS.MessageBox.Show($"Generated patterns.pdf in {directory}");
+                }
             }
         }
     }
