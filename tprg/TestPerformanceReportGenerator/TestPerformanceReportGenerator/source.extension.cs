263c3c7a4bfede950e9722935fb392efb3089481
SimonL26 <806985104@qq.com>
2022-08-30 22:12:46 +0100

    Revert "Revert "Manually merged overview branch to tprg branch since overview branch was broken due to a change to the project name""
    This reverts commit 81614c78613fb0ab20fcdf9d11b059de6d1c7c74.

--- tprg/TestPerformanceReportGenerator/TestPerformanceReportGenerator/source.extension.cs
+++ tprg/TestPerformanceReportGenerator/TestPerformanceReportGenerator/source.extension.cs
@@ -7,12 +7,14 @@ namespace TestPerformanceReportGenerator
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "TestPerformanceReportGenerator.8d73f547-31c3-41db-a78e-d0369731ea87";
-        public const string Name = "TestPerformanceReportGenerator";
-        public const string Description = @"Empty VSIX Project.";
+        public const string Id = "CodeQualityReportGen.8d73f547-31c3-41db-a78e-d0369731ea87";
+        public const string Name = "CodeQualityReportGen";
+        public const string Description = @"This extension generates html reports which reports the code metrics, test outcomes and line coverage. It can also generate an ""../overview"" report which includes all the code metrics and test outcomes from previous reports. 
+The reports will be stored in a directory called ""../Reports"" which can be found in the directory where the solution file (.sln) is stored.
+The overview report will be stored in a directory called ""../Overview_Report"" which can be found in the directory where the solution file (.sln) is stored.Visual Studio Extension that";
         public const string Language = "en-US";
         public const string Version = "1.0";
-        public const string Author = "Simone Lin";
+        public const string Author = "UCLBlueprints";
         public const string Tags = "";
     }
 }
