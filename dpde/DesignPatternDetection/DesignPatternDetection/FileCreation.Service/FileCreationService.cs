746d13983201b8b319017f2b23d71f943430554a
Lama-alluwaymi <63431277+Lama-alluwaymi@users.noreply.github.com>
2022-08-31 18:25:15 +0100

    Added file generation service and added unit tests

---
+++ dpde/DesignPatternDetection/DesignPatternDetection/FileCreation.Service/FileCreationService.cs
@@ -0,0 +1,133 @@
+﻿using Aspose.Cells;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FileCreation.Service
+{
+    public class FileCreationService
+    {
+        public bool WriteJsonData(List<PatternData> patterns, string directory)
+        {
+            try
+            {
+                if (!patterns.Any())
+                {
+                    return false;
+                }
+                if (!Directory.Exists(directory))
+                {
+                    return false;
+                }
+                string jsonData = JsonSerializer.Serialize(patterns, new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText($"{directory}\\patterns.json", jsonData);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool WritePdfData(List<PatternData> patterns, string directory)
+        {
+            try
+            {
+                if (!patterns.Any())
+                {
+                    return false;
+                }
+                if (!Directory.Exists(directory))
+                {
+                    return false;
+                }
+
+                var workbook = new Workbook();
+                var worksheet = workbook.Worksheets[0];
+
+                var dt = new DataTable();
+
+                dt.Columns.Add("Pattern", typeof(string));
+                dt.Columns.Add("Total", typeof(string));
+                dt.Columns.Add("File Name", typeof(string));
+                dt.Columns.Add("Line", typeof(string));
+
+                dt.Rows.Add(dt.NewRow());
+                foreach (var pattern in patterns)
+                {
+                    var dr = dt.NewRow();
+                    dr["Pattern"] = pattern.Pattern;
+                    dr["Total"] = pattern.totalCount.ToString();
+                    dr["File Name"] = string.Empty;
+                    dr["Line"] = string.Empty;
+                    dt.Rows.Add(dr);
+                    foreach (var entry in pattern.Entries)
+                    {
+                        dr = dt.NewRow();
+                        dr["Pattern"] = string.Empty;
+                        dr["Total"] = string.Empty;
+                        dr["File Name"] = entry.FileName;
+                        dr["Line"] = entry.Line;
+                        dt.Rows.Add(dr);
+                    }
+                    dr = dt.NewRow(); //empty row to differentiate
+                    dt.Rows.Add(dr);
+                }
+
+                Cells cells = worksheet.Cells;
+                cells.UnMerge(0, 1, 2, 2);
+                cells.Merge(0, 1, 2, 2);
+                worksheet.Cells[0, 1].PutValue("DESIGN PATTERN REPORT");
+                Style style = worksheet.Cells[0, 1].GetStyle();
+                Aspose.Cells.Font font = style.Font;
+                font.Size = 18;
+                font.IsBold = true;
+                style.Pattern = BackgroundType.Solid;
+                style.HorizontalAlignment = TextAlignmentType.Center;
+                cells[0, 1].SetStyle(style);
+
+                worksheet.Cells.ImportDataTable(dt, true, "A8");
+                worksheet.AutoFitColumn(0);
+                worksheet.AutoFitColumn(1);
+                worksheet.AutoFitColumn(2);
+                worksheet.AutoFitColumn(3);
+                worksheet.PageSetup.Orientation = PageOrientationType.Landscape;
+
+                Style patternStyle = worksheet.Cells[7, 0].GetStyle();
+                Aspose.Cells.Font patternFont = patternStyle.Font;
+                patternFont.IsBold = true;
+                patternStyle.Pattern = BackgroundType.Solid;
+                patternStyle.HorizontalAlignment = TextAlignmentType.Center;
+                cells[7, 0].SetStyle(patternStyle);
+                cells[7, 1].SetStyle(patternStyle);
+                cells[7, 2].SetStyle(patternStyle);
+                cells[7, 3].SetStyle(patternStyle);
+
+                workbook.Save($"{directory}\\patterns.pdf", Aspose.Cells.SaveFormat.Pdf);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+
+    public class PatternData
+    {
+        public string Pattern { get; set; }
+        public int totalCount { get; set; }
+        public List<Entry> Entries { get; set; }
+    }
+
+    public class Entry
+    {
+        public string FileName { get; set; }
+        public int Line { get; set; }
+    }
+}
---
+++ dpde/DesignPatternDetection/DesignPatternDetection/FileCreation.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileCreation.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileCreation.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9e55552-662f-439c-a2e7-5f9b78545361")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
